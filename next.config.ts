e437380d092bf74867718b36f6d173e1adc7c084
SYED175 <syedismailgp@gmail.com>
2025-03-12 13:06:32 +0530

    Update eslint and typescript rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+  },
 };
 
 export default nextConfig;
